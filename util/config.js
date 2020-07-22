c6b913a3b27d33041d56ac1f6d008ce679b960ed
ywymoshi <46644748+ywymoshi@users.noreply.github.com>
2020-07-22 20:51:50 +0800

    first commit

---
+++ util/config.js
@@ -0,0 +1,18 @@
+module.exports ={
+  headers: {
+    'cookie' : 'user_trace_token=20200319233949-f72bf327-f3cb-4e29-9840-aa6b0d2e9e86; _ga=GA1.2.1540699799.1584632388; LGUID=20200319233950-d7ed5c0e-eda7-4469-8325-cf2233d5d2cb; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1591442490; sajssdk_2015_cross_new_user=1; sensorsdata2015session=%7B%7D; LG_LOGIN_USER_ID=d52d764c1f92d1bc6d8337a47303df4c3c2f4b60f3ffbc9138aa1cc777af7e2c; LG_HAS_LOGIN=1; _putrc=C6395FF3F831A514123F89F2B170EADC; login=true; unick=%E6%A8%A1%E5%BC%8F; gate_login_token=ffd606393e138024c9e872a81cc7d34d7f788364d673d4595ff6f98dca8253e2; X_HTTP_TOKEN=4c002304842db0e28967925951637d70ca32643a44; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2215145583%22%2C%22first_id%22%3A%221736f1e245f76c-009356bbfc5875-3b634404-2073600-1736f1e246045d%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E8%87%AA%E7%84%B6%E6%90%9C%E7%B4%A2%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.baidu.com%2Flink%22%2C%22%24latest_utm_source%22%3A%22PC%20%22%2C%22%24latest_utm_medium%22%3A%22EDUBanner%22%2C%22%24latest_utm_term%22%3A%22java3214%22%2C%22%24latest_utm_campaign%22%3A%22PC-EDU-Banner%22%2C%22%24os%22%3A%22Windows%22%2C%22%24browser%22%3A%22Chrome%22%2C%22%24browser_version%22%3A%2283.0.4103.116%22%7D%2C%22%24device_id%22%3A%221736f1d3103178-0872d8e06e8f7f-3b634404-2073600-1736f1d310464c%22%7D',
+    'accept': 'application/json, text/plain, */*',
+    'accept-encoding': 'gzip, deflate, br',
+    'origin': 'https://kaiwu.lagou.com',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+    'x-l-req-header': '{deviceType:1}',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+  },
+  courseLessonUrl:'https://gate.lagou.com/v1/neirong/kaiwu/getAllCoursePurchasedRecordForPC',
+  courseUrl:'https://gate.lagou.com/v1/neirong/kaiwu/getCourseLessons?courseId=',
+  lessonUrl:'https://gate.lagou.com/v1/neirong/kaiwu/getCourseLessonDetail?lessonId=',
+  secretOrkey:'123456'
+}
+
