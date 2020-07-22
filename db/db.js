2cca6c9859a730bf36c1078749f6090f2db55f40
ywymoshi <46644748+ywymoshi@users.noreply.github.com>
2020-07-22 22:32:07 +0800

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const db = require("mongoose");
 
 // 1.连接
-db.connect("mongodb://root:1570555124..@139.199.206.195:27017/lagou", {useNewUrlParser: true,  useUnifiedTopology: true });
+db.connect("mongodb://127.0.0.1:27017/test", {useNewUrlParser: true,  useUnifiedTopology: true });
 
 const conn = db.connection;
 conn.on("error", () => console.error("连接数据库失败"));
